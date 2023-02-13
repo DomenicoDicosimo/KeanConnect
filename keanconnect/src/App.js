d1aee4c2d1dffa02d35c81ec92065dba210870d9
DomenicoDicosimo <65661974+DomenicoDicosimo@users.noreply.github.com>
2023-02-13 16:42:04 -0500

    added firebase

--- keanconnect/src/App.js
+++ keanconnect/src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCLSG8GUInKQ__vZXSa39EC3a7dl9MrMI4",
+  authDomain: "keanconnect-462c0.firebaseapp.com",
+  projectId: "keanconnect-462c0",
+  storageBucket: "keanconnect-462c0.appspot.com",
+  messagingSenderId: "1054788257970",
+  appId: "1:1054788257970:web:b55fdfe15897ac5952e1ab",
+  measurementId: "G-WDTMSRF8FG"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
 }
 
 export default App;
+ 
