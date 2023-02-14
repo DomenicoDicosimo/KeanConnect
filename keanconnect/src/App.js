136bd7f9544d13e5037628708076aa14ef20ab15
DomenicoDicosimo <65661974+DomenicoDicosimo@users.noreply.github.com>
2023-02-13 22:12:40 -0500

    backend build

--- keanconnect/src/App.js
+++ keanconnect/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import React, {useRef, useState} from 'react';
 import './App.css';
 
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
@@ -22,14 +22,103 @@ const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         
       </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
 }
+
+function SignIn(){
+  const useSignInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return(
+    <>
+      <button onClick={useSignInWithGoogle}>Sign in with Google</button>
+    </>
+  )
+}
+
+function SignOut(){
+  return auth.currentUser && (
+    <button onClick ={() => auth.signOut()}> Sign Out</button>
+  )
+}
+
+function ChatRoom(){
+  //pulls the last 25 messages from the chat app
+  const dummy = useRef();
+
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  //reacts to changes in real time
+  const [messages] = useCollectionData(query,{idField: 'id'});
+
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async(e) => {
+    //prevents data being reset on refresh
+    e.preventDefault();
+
+    const { uid, photoURL} = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+    
+    dummy.current.scrollIntoView({behavior: 'smooth'});
+  }
+
+  //loops over each document, passes document data as the message prop
+  //input value binds state to form input
+  return(<>
+    <main>
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+      <div ref={dummy}></div>
+    </main>
+  
+    <form onSubmit={sendMessage}>
+
+      <input value = {formValue} onChange={(e) => setFormValue(e.target.value)} />
+
+      <button type = "submit" disabled={!formValue}>Send</button>
+    </form>
+  </>
+  )
+}
+
+function ChatMessage(props){
+
+  const {text, uid, photoURL} = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+  return (<>
+    <div className = {`message ${messageClass}`}>
+      <img src = {photoURL} />
+      <p>{text}</p>
+    </div>
+  </>)
+}
+
 
 export default App;
  
